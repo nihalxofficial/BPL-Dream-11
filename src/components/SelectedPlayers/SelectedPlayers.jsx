911fcf1931d555d688567d6bb6cc6f87fb0e2601
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-03-28 18:07:08 +0600

    Card designed for selected players

--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { MdDelete } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { MdOutlineSportsCricket } from "react-icons/md";
+
+
 
 const SelectedPlayers = ({selectedPlayers}) => {
     return (
-        <div>
-            {selectedPlayers.map(player=> {
-                return <h2>{player.playerName}</h2>
+        <div className='space-y-5'>
+            {selectedPlayers.map((player, index)=> {
+                return <div key={index} className='flex items-center justify-between gap-6 p-5 rounded-2xl shadow-sm'>
+                    <div className='flex justify-between items-center gap-4'>
+                        <img className='w-20 h-20 ' src={player.playerImg} alt="" />
+                        <div>
+                            <h2 className='font-bold text-xl flex justify-between items-center gap-2'><FaUser /> {player.playerName}</h2>
+                            <p className=' text-gray-600 flex items-center gap-2' ><MdOutlineSportsCricket /> {player.playerType}</p>
+                        </div>
+
+                    </div>
+                    <button>< MdDelete className='cursor-pointer text-2xl text-red-600' /></button>
+                </div>
             })}
         </div>
     );
