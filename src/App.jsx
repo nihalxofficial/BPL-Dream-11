5174705d1ad183583f207a0070a11e16859231ff
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-03-28 17:43:26 +0600

    Loading design updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Suspense, useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Players from './components/Players/Players';
+import Loading from './components/Loading/Loading';
 
 const fetchPlayer = async() => {
   const res = await fetch("/data.json")
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className='space-y-8'>
       <Navbar coin={coin}></Navbar>
-      <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+      <Suspense fallback={<Loading></Loading>} >
 
       <Players coin={coin} setCoin={setCoin} playerPromise = {playerPromise}></Players>
       </Suspense>
