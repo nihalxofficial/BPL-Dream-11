796cdc35d490e158f22949d4b17a770bfbdacba1
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-03-28 19:00:59 +0600

    No player selected section added

--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -16,7 +16,7 @@ const SelectedPlayers = ({selectedPlayers,setSelectedPlayers, coin, setCoin}) =>
     }
     return (
         <div className='space-y-5'>
-            {selectedPlayers.map((player, index)=> {
+            {selectedPlayers.length >= 1 ? selectedPlayers.map((player, index)=> {
                 return <div key={index} className='flex items-center justify-between gap-6 p-5 rounded-2xl shadow-sm'>
                     <div className='flex justify-between items-center gap-4'>
                         <img className='w-20 h-20 ' src={player.playerImg} alt="" />
@@ -28,7 +28,12 @@ const SelectedPlayers = ({selectedPlayers,setSelectedPlayers, coin, setCoin}) =>
                     </div>
                     <button onClick={()=>deleteSelectedPlayer(player)}>< MdDelete className='cursor-pointer text-2xl text-red-600' /></button>
                 </div>
-            })}
+            })
+            : <div className='h-100 flex justify-center items-center flex-col gap-3'>
+                <h2 className='font-semibold text-xl'>No players selected yet</h2>
+                <p>Go to available tab to select players</p>
+            </div>
+            }
         </div>
     );
 };
